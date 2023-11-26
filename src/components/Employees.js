de55d6125a72eabcc61b8b294bf610c95d22c95d
mail2devikad <mail2devikad@gmail.com>
2023-11-26 23:20:48 +0530

    React crud

---
+++ src/components/Employees.js
@@ -0,0 +1,16 @@
+const Employees=[
+    {
+        id:"1",
+        Name:"Devika ",
+        Age:"24"
+    },{
+        id:"2",
+        Name:"Geethu",
+        Age:"26"
+    },{
+        id:"3",
+        Name:"Aswin",
+        Age:"27"
+    }
+]
+export default Employees;
